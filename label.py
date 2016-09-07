926cad105a60c00e6a1e9dfe3afa437478a95cc7
sakup <pukasbohdan@gmail.com>
2016-09-07 21:59:33 +0300

    begining

---
+++ label.py
@@ -0,0 +1,17 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+kivy.require ("1.9.1")
+
+class MyWindowApp(App):
+    def build(self):
+        lbl = Label(text='WTF')
+        return lbl
+
+window = MyWindowApp()
+window.run()
+
+#comment
+#comment2
+
+#new comment
